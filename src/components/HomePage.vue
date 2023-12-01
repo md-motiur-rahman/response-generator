07ee0f9481e047472d8032f374ec31e504c1bcc6
md-motiur-rahman <cse.motiur@gmail.com>
2023-12-01 02:00:56 +0000

    project init

---
+++ src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+    <NavBar />
+    <div>
+        <img alt="Vue logo" src="../assets/logo.png">
+        <h1>Home</h1>
+    </div>
+</template>
+<script>
+import NavBar from './NavBar.vue'
+export default {
+    name: 'HomePage',
+    components: {
+        NavBar
+    }
+}
+</script>
+<style lang="">
+    
+</style>
